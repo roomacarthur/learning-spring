a645113175227f2dc2e7136fe15b6c4595adc09a
roomacarthur <roomacarthur@gmail.com>
2024-06-26 12:52:35 +0100

    Initial commit

---
+++ src/main/java/com/roomac/lil/leaning_spring/LeaningSpringApplication.java
@@ -0,0 +1,13 @@
+package com.roomac.lil.leaning_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaningSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roomac/lil/leaning_spring/LeaningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomac.lil.leaning_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
